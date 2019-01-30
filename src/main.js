aecc5ea655db9f853322574f5bd65a3bf29e60f0
bobdoah <robertwilliams1985@gmail.com>
2019-01-30 18:17:42 +0000

    Added the firebase SDK and a function to generate image thumbnails.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import firebase from "firebase";
 import App from "./App.vue";
 import router from "./router";
 import Buefy from "buefy";
@@ -8,6 +9,16 @@ require("@/assets/css/instaAlbum.css");
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
+
+var config = {
+  apiKey: "AIzaSyAB_yBu6uCOLAD3ci1Cf-5H6sh_nuGbMBo",
+  authDomain: "sarah-and-rob.firebaseapp.com",
+  databaseURL: "https://sarah-and-rob.firebaseio.com",
+  projectId: "sarah-and-rob",
+  storageBucket: "sarah-and-rob.appspot.com",
+  messagingSenderId: "793325439550"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
