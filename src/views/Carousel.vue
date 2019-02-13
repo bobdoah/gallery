061f3c0facae902b5028d1aeb579b55d8ff19b42
Robert Williams <robertwilliams1985@gmail.com>
2019-02-13 22:35:13 +0000

    Added the beginnings of a carousel view

---
+++ src/views/Carousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <agile
+    :arrows="false"
+    :speed="750"
+    :timing="'linear'"
+    :fade="true"
+    :autoplay="true"
+    :pauseOnHoover="false"
+  >
+    <div
+      v-for="image of images.slice().reverse()"
+      :key="image.key"
+      class="slide"
+    >
+      <figure class="image">
+        <img :src="image.thumbnail" />
+      </figure>
+    </div>
+  </agile>
+</template>
+<script>
+import { db } from "../firebase";
+export default {
+  data() {
+    return {
+      images: []
+    };
+  },
+  firebase: {
+    images: db.ref("images").orderByKey()
+  }
+};
+</script>
