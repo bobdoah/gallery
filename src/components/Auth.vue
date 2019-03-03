facb3b2c0849ee6dd0c0d3f80403611859d1d42b
Robert Williams <robertwilliams1985@gmail.com>
2019-03-03 21:27:30 +0000

    Add facebook auth and lock down the database rules so only authenticated users can get the entries

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -16,6 +16,7 @@ export default {
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         {
           provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
           recaptchaParameters: {
